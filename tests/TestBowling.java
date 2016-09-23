aebb16430e1c3969b809e75f3fb6ed601e73f6ee
somename <someemail>
2016-09-23 15:00:55 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -9,25 +9,25 @@ public class TestBowling {
 
 	@Before
 	public void initializeGame() throws BowlingException {
-		Frame oneFrame = new Frame(1, 2);
+		Frame oneFrame = new Frame(1, 5);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(3, 6);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(7, 2);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(3, 6);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(4, 4);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(5, 3);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(3, 3);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(4, 5);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(8, 1);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+		oneFrame = new Frame(2, 6);
 		oneGame.addFrame(oneFrame);
 	}
 
