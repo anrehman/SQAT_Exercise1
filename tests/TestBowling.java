b3ea1cc22dfc26f8a25ada5d40591222c3df2801
somename <someemail>
2016-09-23 14:59:48 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -1,13 +1,14 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestBowling {
 
 	BowlingGame oneGame = new BowlingGame();
 
-	@Test(expected = BowlingException.class)
-	public void testBowling_testFrameSize() throws BowlingException {
+	@Before
+	public void initializeGame() throws BowlingException {
 		Frame oneFrame = new Frame(1, 2);
 		oneGame.addFrame(oneFrame);
 		oneFrame = new Frame(1, 2);
@@ -28,14 +29,23 @@ public class TestBowling {
 		oneGame.addFrame(oneFrame);
 		oneFrame = new Frame(1, 2);
 		oneGame.addFrame(oneFrame);
-		oneFrame = new Frame(1, 2);
+	}
+
+	@Test(expected = BowlingException.class)
+	public void testBowling_testFrameSize() throws BowlingException {
+		Frame oneFrame = new Frame(1, 2);
 		oneGame.addFrame(oneFrame);
 	}
-	
+
 	@Test
 	public void testBowling_singleFrameSum() {
 		Frame oneFrame = new Frame(2, 3);
-		assertEquals("Single Frame Score Test: ",oneFrame.score(),5);
+		assertEquals("Single Frame Score Test: ", oneFrame.score(), 5);
+	}
+
+	@Test
+	public void testBowling_gameScoreCheck() {
+
 	}
 
 }
